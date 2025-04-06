d4d281cd6da7e3e564470f553a8ba8868c18554d
jaydeepkumar1984 <chovatia.jaydeep@gmail.com>
2025-04-06 14:15:36 -0700

    Create AutoRepair executors only if enabled and a few other nits

--- test/unit/org/apache/cassandra/repair/autorepair/AutoRepairParameterizedTest.java
+++ test/unit/org/apache/cassandra/repair/autorepair/AutoRepairParameterizedTest.java
@@ -179,8 +179,8 @@ public class AutoRepairParameterizedTest extends CQLTester
         Keyspace.open(SchemaConstants.DISTRIBUTED_KEYSPACE_NAME).getColumnFamilyStore(SystemDistributedKeyspace.AUTO_REPAIR_PRIORITY).truncateBlocking();
         Keyspace.open(SchemaConstants.DISTRIBUTED_KEYSPACE_NAME).getColumnFamilyStore(SystemDistributedKeyspace.AUTO_REPAIR_HISTORY).truncateBlocking();
 
-
-        AutoRepair.instance = new AutoRepair();
+        AutoRepair.instance.isSetupDone = false;
+        AutoRepair.instance.setup();
         executeCQL();
 
         timeFuncCalls = 0;
@@ -237,24 +237,6 @@ public class AutoRepairParameterizedTest extends CQLTester
         Keyspace.open(SchemaConstants.DISTRIBUTED_KEYSPACE_NAME)
                 .getColumnFamilyStore(SystemDistributedKeyspace.AUTO_REPAIR_PRIORITY)
                 .forceBlockingFlush(ColumnFamilyStore.FlushReason.UNIT_TESTS);
-    }
-
-    @Test(expected = ConfigurationException.class)
-    public void testRepairAsyncWithRepairTypeDisabled()
-    {
-        AutoRepairService.instance.getAutoRepairConfig().setAutoRepairEnabled(repairType, false);
-
-        AutoRepair.instance.repairAsync(repairType);
-    }
-
-    @Test
-    public void testRepairAsync()
-    {
-        AutoRepair.instance.repairExecutors.put(repairType, mockExecutor);
-
-        AutoRepair.instance.repairAsync(repairType);
-
-        verify(mockExecutor, times(1)).submit(any(Runnable.class));
     }
 
     @Test
